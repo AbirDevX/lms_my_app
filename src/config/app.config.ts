951683904a84162c688c6e71cd497ff145396360
Abir Devx <abir.devx@gmail.com>
2025-11-04 22:56:02 +0530

    new

---
+++ src/config/app.config.ts
@@ -0,0 +1,18 @@
+// export default () => ({
+//   PORT: process.env.PORT || 'default',
+//   THROTTLE_TTL: process.env.THROTTLE_TTL,
+//   THROTTLE_LIMIT: process.env.THROTTLE_LIMIT,
+//   DB_USERNAME: process.env.DB_USERNAME,
+//   DB_PASSWORD: process.env.DB_PASSWORD,
+//   DB_DATABASE: process.env.DB_DATABASE,
+//   DB_CLIENT: process.env.DB_CLIENT,
+//   DB_PORT: process.env.DB_PORT,
+//   DB_HOST: process.env.DB_HOST,
+
+//   JWT_ACCESS_TOKEN_SECRET: process.env.JWT_ACCESS_TOKEN_SECRET,
+//   JWT_REFRESH_TOKEN_SECRET: process.env.JWT_REFRESH_TOKEN_SECRET,
+
+//   CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
+//   CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+//   CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+// });
